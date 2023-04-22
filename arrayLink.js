0f3fab59e7b6d7a26b6bce563fbdec1d30d42284
DucPhamH <ducphamhong2@gmail.com>
2023-04-22 11:04:42 +0700

    craw data

---
+++ arrayLink.js
@@ -0,0 +1,7 @@
+let arrayLink = [];
+for (let i = 1; i <= 5; i++) {
+  const url = `https://monngonmoingay.com/tim-kiem-mon-ngon/page/${i}/`;
+  arrayLink.push(url);
+}
+
+module.exports = arrayLink;
--- index.js
+++ index.js
@@ -14,35 +14,117 @@ app.use(morgan("combined"));
 const rp = require("request-promise");
 const cheerio = require("cheerio");
 const fs = require("fs");
+const arrayLink = require("./arrayLink");
+console.log(arrayLink);
 
-const URL = `https://monngonmoingay.com/lau-vit-tia-to/`;
+// const URL = `https://monngonmoingay.com/tim-kiem-mon-ngon/page/1/`;
 
-const options = {
-  uri: URL,
-  transform: function (body) {
-    //Khi lấy dữ liệu từ trang thành công nó sẽ tự động parse DOM
-    return cheerio.load(body);
-  },
-};
+// console.log(URL);
 
-(async function crawler() {
-  try {
-    // Lấy dữ liệu từ trang crawl đã được parseDOM
-    var $ = await rp(options);
-  } catch (error) {
-    return error;
+// let data = [];
+
+// const options = {
+//   uri: URL,
+//   transform: function (body) {
+//     //Khi lấy dữ liệu từ trang thành công nó sẽ tự động parse DOM
+//     return cheerio.load(body);
+//   },
+// };
+
+// (async function crawler() {
+//   try {
+//     // Lấy dữ liệu từ trang crawl đã được parseDOM
+//     var $ = await rp(options);
+//   } catch (error) {
+//     return error;
+//   }
+
+//   const title = $(".list-recipes > div");
+
+//   for (let i = 0; i < title.length; i++) {
+//     const infoFood = $(title[i]);
+//     const linkFood = infoFood.find(".info-list").find("a").attr("href");
+//     const titleFood = infoFood.find(".info-list").find("a").text().trim();
+//     let linkImage = infoFood
+//       .find(".text-center")
+//       .find(".img-fluid")
+//       .attr("data-lazy-src");
+//     // console.log(titleFood);
+//     if (linkImage === undefined) {
+//       console.log(linkImage);
+//       linkImage =
+//         "https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg";
+//     }
+
+//     data.push({
+//       linkFood,
+//       titleFood,
+//       linkImage,
+//     });
+//   }
+//   console.log(data);
+
+//   // cứ thêm dòng này khi chạy npm start là nó cứ reset đi reset lại
+//   const dataJson = JSON.stringify(data);
+//   fs.writeFileSync("data.json", dataJson);
+// })();
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function crawler() {
+  await sleep(1000);
+  for (i in arrayLink) {
+    const linkchay = arrayLink[i];
+
+    try {
+      const options = {
+        uri: linkchay,
+        transform: function (body) {
+          //Khi lấy dữ liệu từ trang thành công nó sẽ tự động parse DOM
+          return cheerio.load(body);
+        },
+      };
+      var $ = await rp(options);
+    } catch (error) {
+      console.log("Link dang dung:" + arrayLink[i]);
+      return error;
+    }
+
+    const title = $(".list-recipes > div");
+    let data = [];
+
+    for (let i = 0; i < title.length; i++) {
+      const infoFood = $(title[i]);
+      const linkFood = infoFood.find(".info-list").find("a").attr("href");
+      const titleFood = infoFood.find(".info-list").find("a").text().trim();
+      let linkImage = infoFood
+        .find(".text-center")
+        .find(".img-fluid")
+        .attr("data-lazy-src");
+      // console.log(titleFood);
+      if (linkImage === undefined) {
+        console.log(linkImage);
+        linkImage =
+          "https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg";
+      }
+
+      data.push({
+        linkFood,
+        titleFood,
+        linkImage,
+      });
+    }
+    console.log(data);
+
+    fs.writeFileSync("data.json", JSON.stringify(data));
+    console.log(linkchay + "------------->done");
+
+    await sleep(1000);
   }
-
-  const title = $(".block-nguyenlieu ul li").text().trim();
-  console.log(title);
-
-  // test thử data
-  const data = {
-    duc: "duc",
-  };
-  // cứ thêm dòng này khi chạy npm start là nó cứ reset đi reset lại
-  fs.writeFileSync("data.json", JSON.stringify(data));
-})();
+}
+//call crawler
+crawler();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
